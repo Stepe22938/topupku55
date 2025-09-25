e29c5e5b569f76945ed9971c921afc3bffa296a8
Stepe22938 <123631481+Stepe22938@users.noreply.github.com>
2025-09-25 18:06:59 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,7 @@
+
+
+global.username = "barmodssofficial"
+global.passwd = "barmodss123@"
+global.feenya = 50 // FEE PRODUK
+global.apikey = "la26f4f6jtuigvkb" // APIKEY DARI V-PEDIA
+global.mongodb = "mongodb+srv://barrxxxx:barrxxxx@cluster0.pjozlkg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" // JANGAN DI UBAH INI DB
